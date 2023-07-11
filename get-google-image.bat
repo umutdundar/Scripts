fb61b2888700a6bfa7371f679395ba1903d89dc8
umutdundar <mtdndr@gmail.com>
2023-07-11 14:26:10 +0300

    Update get-google-image.bat

--- get-google-image.bat
+++ get-google-image.bat
@@ -1,8 +1,8 @@
 @title Google Image Downloader
 
 @rem echo off
-@rem usage get-google-image.bat input.txt
-@rem googleimagesdownload -k "what_you_look_for" -l 250 -s ">4MP" -cd path-to-chromedriver/chromedriver.exe
+@rem script  --> get-google-image.bat input.txt
+@rem command --> googleimagesdownload -k "what_you_look_for" -l 250 -s ">4MP" -cd path-to-chromedriver/chromedriver.exe
 @rem size = 2,4,6,8,10,12,15,20,40,70
 
 set limit=100
