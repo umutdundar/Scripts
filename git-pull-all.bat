18cdbec8eea89652f6ceff696879ae0edad69084
umutdundar <mtdndr@gmail.com>
2024-09-06 09:58:42 +0300

    1

--- git-pull-all.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-
-rem for /f %%f in ('dir /ad /b .\') do (cd /d .\%%f & call git reset head --hard & call git pull & cd ..)
-
-for /f %%f in ('dir /ad /b .\') do (echo %%f & cd /d .\%%f & call git pull & cd .. & echo. )
