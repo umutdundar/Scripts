5dab6f11567f02a417ba6c4c738ced3e8c70d7b0
Umut Dündar <mtdndr@gmail.com>
2024-09-06 09:30:26 +0300

    Update git-pull-all.bat

--- git-pull-all.bat
+++ git-pull-all.bat
@@ -2,4 +2,4 @@
 
 rem for /f %%f in ('dir /ad /b .\') do (cd /d .\%%f & call git reset head --hard & call git pull & cd ..)
 
-for /f %%f in ('dir /ad /b .\') do (echo %%f & cd /d .\%%f & call git pull & cd ..)
+for /f %%f in ('dir /ad /b .\') do (echo %%f & cd /d .\%%f & call git pull & cd .. & echo. )
